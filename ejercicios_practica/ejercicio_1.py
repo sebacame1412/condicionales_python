44501dff7213974557adf136d9464052800f33b1
sebacame1412 <105613853+sebacame1412@users.noreply.github.com>
2022-05-28 16:03:00 -0300

    Ejercicio Nro 1 Finalizado
    Completado

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -12,6 +12,9 @@
 # Comparadores
 # Ingrese dos números cualesquiera y realice las sigueintes
 # comparaciones entre ellos
+
+
+
 numero_1 = int(input('Ingrese el primer número:\n'))
 
 numero_2 = int(input('Ingrese el segundo número:\n'))
@@ -19,12 +22,43 @@ numero_2 = int(input('Ingrese el segundo número:\n'))
 # Compare cual de los dos números es mayor
 # Imprima en pantalla según corresponda
 
+if numero_1 > numero_2:
+    print(f"El numero {numero_1} es mayor a {numero_2}")
+
+elif numero_2 < numero_1:
+    print(f"El numero {numero_2} es mayor a {numero_1}")
+
+else:
+    print("Los numeros son iguales")
+
 # Verifique si el numero_1 positivo, negativo o cero
 # Imprima el resultado en cada caso
+
+if numero_1 > 0:
+    print("El numero", (numero_1), "es positivo")
+
+elif numero_1 < 0:
+    print("El numero", (numero_1), "es negativo")
+
+else:
+    print("el numero", (numero_1), "es igual a 0")
 
 # Verifique si el numero_1 es mayor a 0 y menor a 100
 # Imprima en pantalla si se cumple o no la condición
 
+if numero_1 > 0 and numero_1 < 100:
+    print("El numero", (numero_1), "cumple la condición")
+
+else:
+    print("El numero", (numero_1), "no cumple la condición")
+
+
 # Verifique si el numero_1 es menor a 10 o el numero_2
 # es mayor a -2
 # Imprima en pantalla si se cumple o no la condición
+
+if numero_1 < 10 or numero_2 > -2:
+    print("Se cumple la condición")
+    
+else:
+    print("No se cumple la condición")
