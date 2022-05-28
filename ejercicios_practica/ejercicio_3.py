516753f92c8909abd766861f31b63352cdf83970
sebacame1412 <105613853+sebacame1412@users.noreply.github.com>
2022-05-28 16:04:01 -0300

    Ejercicio Nro 3 Finalizado
    Completado

--- ejercicios_practica/ejercicio_3.py
+++ ejercicios_practica/ejercicio_3.py
@@ -1,4 +1,4 @@
-# Condicionales [Python]
+ # Condicionales [Python]
 # Ejercicios de práctica
 
 # Autor: Inove Coding School
@@ -23,6 +23,17 @@ numero_2 = -2
 #       --> En caso afirmativo imprima en pantalla "Resp=3"
 #       --> En caso negativo imprima en pantalla "Resp=4"
 
+if numero_1 > 5:
+    if numero_2 > 0:
+        print("Resp=1")
+    else:
+        print("Resp=2")
+elif numero_1 < 5:
+    if numero_2 > 5:
+        print("Resp=3")
+    else:
+         print("Resp=4")
+
 # Verifique la calificación de un estudiante según su
 # puntaje en un examen
 puntaje = 70
@@ -32,6 +43,16 @@ puntaje = 70
 # Si el puntaje es mayor igual a 70 --> imprimir C
 # Si el puntaje es mayor igual a 60 --> imprimir D
 # Si el puntaje es menor a  60      --> imprimir F
-
 # Debe imprimir en pantalla la calificacion
 # Utilizar "if" anidados
+
+if puntaje >= 90:
+    print("A")
+elif puntaje >= 80 and puntaje < 90:
+    print("B")
+elif puntaje >= 70 and puntaje < 80:
+    print("C")
+elif puntaje >= 60 and puntaje < 70:
+    print("D")
+elif puntaje < 60:
+    print("F")
